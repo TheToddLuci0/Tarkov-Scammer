2dc55f6c15b437175bf8c8d4b97c105d58176329
Logan J. Woolery <woolery@iastate.edu>
2021-03-19 00:35:23 -0500

    Make it pretty, and actually show all results.

--- scam.py
+++ scam.py
@@ -1,15 +1,24 @@
 import requests
 import sys
+from time import sleep
+from tabulate import tabulate
 
 
 def get_scams(api_key):
+    scams = []
     headers = {"x-api-key": api_key}
     r = requests.get('https://tarkov-market.com/api/v1/items/all', headers=headers)
     for i in r.json():
-        data = requests.get('https://tarkov-market.com/api/v1/item?uid='+i['uid'], headers=headers).json()[0]
-        #if data['traderPrice'] > data["price"] or data['traderPrice'] > data["avg24hPrice"] or data['traderPrice'] > data["avg7daysPrice"]:
+        r2 = requests.get('https://tarkov-market.com/api/v1/item?uid='+i['uid'], headers=headers)
+        while r2.status_code == 429:
+            print("Got rate limited, sleeping")
+            sleep(15)
+            r2 = requests.get('https://tarkov-market.com/api/v1/item?uid=' + i['uid'], headers=headers)
+        data = r2.json()[0]
         if data['traderPrice'] > data["price"]:
-            print("BUY {item} AND SELL TO {merch} FOR {p} PROFIT".format(item=i['name'], merch=data['traderName'], p=data['traderPrice'] - data['price']))
+            scams.append([i['name'], data['price'], data['traderName'], data['traderPrice']-data['price']])
+    scams.sort(key=lambda x: x[3])
+    print(tabulate(scams, headers=["Item", "Market Price", "Trader", "Profit"]))
 
 
 if __name__=='__main__':
@@ -19,4 +28,3 @@ if __name__=='__main__':
     except IOException:
         secret = sys.argv[1]
     get_scams(secret)
-        
